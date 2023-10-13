d3e9582bc91c923b51d1355fba42834f11479ddc
VictorG <victor.germain@isep.eleve.fr>
2023-10-13 23:45:41 +0200

    Modification Chess et Cell

--- src/Chess.java
+++ src/Chess.java
@@ -1,5 +1,5 @@
-import Pieces.Cell;
-import Pieces.Player;
+import Pieces.*;
+
 import java.util.Scanner;
 
 public class Chess {
@@ -36,7 +36,40 @@ public class Chess {
         players[1]=player2;
     }
     private void initialiseBoard(){
-        //create board
+        board = new Cell[8][8];
+        Pawn pawnw1 = new Pawn(0,new Position('a',1));
+        Pawn pawnw2 = new Pawn(0,new Position('b',1));
+        Pawn pawnw3 = new Pawn(0,new Position('c',1));
+        Pawn pawnw4 = new Pawn(0,new Position('d',1));
+        Pawn pawnw5 = new Pawn(0,new Position('e',1));
+        Pawn pawnw6 = new Pawn(0,new Position('f',1));
+        Pawn pawnw7 = new Pawn(0,new Position('g',1));
+        Pawn pawnw8 = new Pawn(0,new Position('h',1));
+        Rook rookw1 = new Rook(0,new Position('a',0));
+        Rook rookw2 = new Rook(0,new Position('h',0));
+        Knight knightw1 = new Knight(0,new Position('b',0));
+        Knight knightw2 = new Knight(0,new Position('g',0));
+        Bishop bishopw1 = new Bishop(0,new Position('c',0));
+        Bishop bishopw2 = new Bishop(0,new Position('f',0));
+        King kingw = new King(0,new Position('e',0));
+        Queen queenw = new Queen(0,new Position('d',0));
+
+        Pawn pawnb1 = new Pawn(0,new Position('a',7));
+        Pawn pawnb2 = new Pawn(0,new Position('b',7));
+        Pawn pawnb3 = new Pawn(0,new Position('c',7));
+        Pawn pawnb4 = new Pawn(0,new Position('d',7));
+        Pawn pawnb5 = new Pawn(0,new Position('e',7));
+        Pawn pawnb6 = new Pawn(0,new Position('f',7));
+        Pawn pawnb7 = new Pawn(0,new Position('g',7));
+        Pawn pawnb8 = new Pawn(0,new Position('h',7));
+        Rook rookb1 = new Rook(0,new Position('a',6));
+        Rook rookb2 = new Rook(0,new Position('h',6));
+        Knight knightb1 = new Knight(0,new Position('b',6));
+        Knight knightb2 = new Knight(0,new Position('g',6));
+        Bishop bishopb1 = new Bishop(0,new Position('c',6));
+        Bishop bishopb2 = new Bishop(0,new Position('f',6));
+        King kingb = new King(0,new Position('e',6));
+        Queen queenb = new Queen(0,new Position('d',6));
         //place pieces in starting position ?
     }
     private void printBoard(){}
--- src/Pieces/Cell.java
+++ src/Pieces/Cell.java
@@ -5,7 +5,9 @@ import Pieces.Position;
 public class Cell {
     final Position position;
     boolean isEmpty;
-    public Cell(Position position){
+
+    public Cell(Position position, boolean isEmpty) {
         this.position = position;
+        this.isEmpty = isEmpty;
     }
 }
