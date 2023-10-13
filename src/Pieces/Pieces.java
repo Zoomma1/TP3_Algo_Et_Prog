149edf5b9205456eece46a8fcab0407697c49209
Zoomma1 <victorgerm1@hotmail.fr>
2023-10-13 11:22:53 +0200

    13/10/2023

---
+++ src/Pieces/Pieces.java
@@ -0,0 +1,11 @@
+package Pieces;
+
+public class Pieces {
+    int color;
+    Position position;
+
+    public Pieces(int color, Position position) {
+        this.color = color;
+        this.position = position;
+    }
+}
